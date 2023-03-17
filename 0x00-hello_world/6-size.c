89b304c6c7e63047a4c2cbbeff4565f7d52a89ed
HarryRayManda <dillingercowboy@gmail.com>
2023-03-16 17:03:34 -0700

    sizeoff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,10 @@ long int c;
 long long int d;
 float f;
 
-printf("size of char: %lu byte(s)\n",(unsigned long)sizeof(a));
-printf("size of char: %lu byte(s)\n",(unsigned long)sizeof(b));
-printf("size of char: %lu byte(s)\n",(unsigned long)sizeof(c));
-printf("size of char: %lu byte(s)\n",(unsigned long)sizeof(d));
-printf("size of char: %lu byte(s)\n",(unsigned long)sizeof(f));
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
