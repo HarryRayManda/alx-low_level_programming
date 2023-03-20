4b53efb210d1450bd09acd291cd6810ad1f31b7d
HarryRayManda <dillingercowboy@gmail.com>
2023-03-20 06:32:08 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,7 @@
 #include <unistd.h>
 /**
- * main - Entry point
- * and that piece of art is useful - Dora Korpar, 2015-10-19
- * followed by a new line, to the standard error.
- * return: 0 (success).
+ * main -a program that prints a line with write function
+ *Return:0 (success)
  */
 int main(void)
 {
