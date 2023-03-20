2c5c4c4245ca091a26a25c6e1ad28f9e4faf4ceb
HarryRayManda <dillingercowboy@gmail.com>
2023-03-20 04:47:48 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <unistd.h>
 /**
- * main - A program that  uses a write function "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line,to the standard error.
- * return : Always 1
+ * main - A program that prints a line with write function
+ * return: 1
  */
 int main(void)
 {
