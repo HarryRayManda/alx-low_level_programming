00173e850ad33cf8695200587d3b562b495e6a54
HarryRayManda <dillingercowboy@gmail.com>
2023-03-20 06:09:02 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - Entry point
+ * "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
  * return: Always 1.
  */
