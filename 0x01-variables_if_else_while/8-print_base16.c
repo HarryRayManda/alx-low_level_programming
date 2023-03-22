3349a1ba4a6a28df44e86488aca211625d33b2a1
HarryRayManda <dillingercowboy@gmail.com>
2023-03-22 08:48:38 -0700

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,10 @@ int main(void)
 
 int i;
 char low
+
 for (i = '0'; i <= '9'; i++)
 putchar(i);
-for (low = 'a';low <= 'f'; low++)
+for (low = 'a'; low <= 'f'; low++)
 putchar(low);
 putchar('\n');
 
