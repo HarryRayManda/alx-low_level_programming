8a49744a7505dde4c97761041ba91c48f0f69161
HarryRayManda <dillingercowboy@gmail.com>
2023-03-22 08:44:17 -0700

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Alaways 0 (success)
+ *
+ */
+int main(void)
+{
+
+int i;
+char low
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+for (low = 'a';low <= 'f'; low++)
+putchar(low);
+putchar('\n');
+
+return (0);
+}
