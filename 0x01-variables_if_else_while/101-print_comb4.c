fb0650c39ba30dec0bd9860be44815c6ce9919a9
HarryRayManda <dillingercowboy@gmail.com>
2023-03-22 10:57:26 -0700

    updated

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Alaways 0 (success)
+ *
+ */
+int main(void)
+{
+int i, h, b;
+for (i = '0'; i < '9'; i++)
+{
+for (h = i + 1; h <= '9'; h++)
+{
+for (b = h + 1; b <= '9'; b++)	
+{	
+if ((h != i) !=b)
+{
+putchar(i);
+putchar(h);
+putchar(b);
+if (i == '7' && h == '7')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
