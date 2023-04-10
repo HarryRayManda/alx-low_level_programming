a610fdea32afdc5fcc09dea5a91d2aa09b285fbf
HarryRayManda <dillingercowboy@gmail.com>
2023-04-09 23:43:48 -0700

    create_static_lib.sh

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-puts("\"At the end of the day,be the best hacker\"\n\t- Kevin Mitnick");
+_puts("\"At the end of the day,be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
