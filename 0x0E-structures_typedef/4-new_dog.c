ad943003a88252c62ad2b462871f4296752be6e2
HarryRayManda <dillingercowboy@gmail.com>
2023-04-17 16:43:53 -0700

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,6 @@
 int _strlen(char *s)
 {
 int i;
-
 i = 0;
 while (s[i] != '\0')
 {
@@ -31,27 +30,20 @@ return (i);
 *
 * Return: the pointer to dest
 */
+
 char *_strcpy(char *dest, char *src)
 {
 int len, i;
-
-
 len = 0;
-
-
 while (src[len] != '\0')
 {
 len++;
 }
-
-
 for (i = 0; i < len; i++)
 {
 dest[i] = src[i];
 }
 dest[i] = '\0';
-
-
 return (dest);
 }
 
@@ -69,14 +61,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *dog;
 int len1, len2;
-
-
 len1 = _strlen(name);
 len2 = _strlen(owner);
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
 return (NULL);
-
 
 dog->name = malloc(sizeof(char) * (len1 + 1));
 if (dog->name == NULL)
@@ -94,7 +83,5 @@ return (NULL);
 _strcpy(dog->name, name);
 _strcpy(dog->owner, owner);
 dog->age = age;
-
-
 return (dog);
 }
